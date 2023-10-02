e7647e256e3deb88abaaf3a96543d16f1f435899
achraf-boug <achraf.bougadir@gmail.com>
2023-10-02 20:43:28 +0100

    first_assignement

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with clear suntax"
+str = " ".join([str[39:66],str[-17:-13],str[:6]])
+print(str)
