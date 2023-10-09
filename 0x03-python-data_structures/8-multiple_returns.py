824beec44f7e00a5fccd109c33b3bfeb2c2e207c
achraf-boug <achraf.bougadir@gmail.com>
2023-10-09 23:56:49 +0100

    correct

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,6 @@ def multiple_returns(sentence):
     my_tuple = ()
     if len(sentence) == 0:
         my_tuple = 0, "None"
-    my_tuple = len(sentence), sentence[0]
+    else :
+        my_tuple = len(sentence), sentence[0]
     return my_tuple
